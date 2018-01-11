fefe46ebd6bfc9191d865c792f10c3cc353980f2
Dean <deanw@starcomsoft.com>
2018-01-11 13:55:18 -0800

    Another conflict, this time for rebasing

--- zHello/src/main/java/one/basic/HelloWorld.java
+++ zHello/src/main/java/one/basic/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 
 	public static void main(String[] aregs) {
 		// do something nobody wanted
-		System.out.println("Hello World???");
+		System.out.println("Hello World!???");
 	}
 }
