90477c667c22f0d0bf61156904cc63ad2e025717
Dean <deanw@starcomsoft.com>
2018-01-31 08:57:27 -0800

    Adding a new paramater

--- zHello/src/main/java/one/basic/domain/result/Genotype.java
+++ zHello/src/main/java/one/basic/domain/result/Genotype.java
@@ -4,13 +4,21 @@ public class Genotype {
 
   private String category;
 
-  
+  private double confidence;
+
   public String getCategory() {
     return category;
   }
 
-  
   public void setCategory(String category) {
     this.category = category;
+  }
+
+  public double getConfidence() {
+    return confidence;
+  }
+
+  public void setConfidence(double confidence) {
+    this.confidence = confidence;
   }
 }
