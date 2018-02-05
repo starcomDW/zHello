5e589f19647596cf3eb3e666f14aef74e9b50085
Dean <deanw@starcomsoft.com>
2018-02-05 13:00:40 -0800

    Adding unknown base type and Genotype name

--- zHello/src/main/java/one/basic/domain/result/Genotype.java
+++ zHello/src/main/java/one/basic/domain/result/Genotype.java
@@ -2,14 +2,22 @@ package one.basic.domain.result;
 
 public class Genotype {
 
+  private String name;
+
   private String category;
 
-  
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
   public String getCategory() {
     return category;
   }
 
-  
   public void setCategory(String category) {
     this.category = category;
   }
