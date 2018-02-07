f8d97eea7e7df7b56dd1de396ca946f6aaad599c
Dean <deanw@starcomsoft.com>
2018-02-07 09:00:49 -0800

    Adding locus to pcr

--- zHello/src/main/java/one/basic/PCR.java
+++ zHello/src/main/java/one/basic/PCR.java
@@ -1,9 +1,21 @@
 package one.basic;
 
+import one.basic.domain.Locus;
+
 public class PCR extends Experiment {
 
-  public PCR(String name) {
-    super(name);
-  }
+	private Locus locus;
+
+	public PCR(String name) {
+		super(name);
+	}
+
+	public Locus getLocus() {
+		return locus;
+	}
+
+	public void setLocus(Locus locus) {
+		this.locus = locus;
+	}
 
 }
