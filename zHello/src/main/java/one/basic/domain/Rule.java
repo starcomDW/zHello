d4fe40f42ed5ec8eda51a4677011dccbdc12e027
Dean <deanw@starcomsoft.com>
2018-02-07 09:22:49 -0800

    Adding a name

--- zHello/src/main/java/one/basic/domain/Rule.java
+++ zHello/src/main/java/one/basic/domain/Rule.java
@@ -2,4 +2,14 @@ package one.basic.domain;
 
 public class Rule {
 
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
