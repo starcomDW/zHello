54f8581e2af2d4b0e6e112fc1253db22b1fb2668
Dean <deanw@starcomsoft.com>
2018-02-15 10:50:16 -0800

    Testing synchronize

--- zHello/src/main/java/one/basic/Experiment.java
+++ zHello/src/main/java/one/basic/Experiment.java
@@ -1,13 +1,30 @@
 package one.basic;
 
+import java.util.*;
+
 public class Experiment {
 
   private String name;
+  
+  private Date date;
 
   public Experiment(String name) {
     this.name = name;
   }
   
+  public Date getDate() {
+    return date;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+  
+  public String getName() {
+    return name;
+  }
+
   @Override
   public String toString() {
     return "Experiment [name=" + name + "]";
